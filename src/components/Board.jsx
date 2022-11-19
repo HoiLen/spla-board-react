1da60f092b73f47e83eba5b583ec10742ee5fa84
HoiLen <len1231.hoi@gmail.com>
2022-11-19 19:09:40 +0900

    drag and drop

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,9 +4,20 @@ const Board = () => {
 	const ref = useRef(null);
 
 	//要素の位置を状態として記憶する
-	const [pos, setPos] = useState({ x: 0, y: 0 });
+	//const [pos, setPos] = useState({ x: 0, y: 0 });
+	const [dragging, setDragging] = useState({ key: "", x: 0, y: 0 });
+	const [cards, setCards] = useState({
+		id1: { t: "a", x: 100, y: 100 },
+		id2: { t: "b", x: 200, y: 100 }
+	});
+
+	const update = (key, card) => setCards({ ...cards, [key]: card });
+
+	const [editMode, setEditMode] = useState({ key: "" });
+
 
 	return (
+
 		<div
 			style={{
 				width: "1000px",
@@ -14,26 +25,45 @@ const Board = () => {
 				position: "relative"
 			}}
 			onDrop={
-				(e) => setPos({
-					x: e.clientX - ref.current.clientWidth/2,
-					y: e.clientY - ref.current.clientHeight/2
-				})
+				(e) => {
+					if (!dragging || !cards) return;
+					update(dragging.key, {
+						...cards[dragging.key],
+						x:e.clientX - dragging.x,
+						y:e.clientY - dragging.y
+					});
+				}
 			}
 			onDragOver={
 				(e) => e.preventDefault()
 			}
 		>
-			<div
-				style={{
-					position: "absolute",
-					top: pos.y + "px",
-					left: pos.x + "px",
-				}}
-				draggable={true}
-				ref={ref}
-			>
-				Prats
-			</div>
+			{Object.keys(cards).map((key) => (
+				<div
+					key={key}
+					style={{
+						position: "absolute",
+						top: cards[key].y + "px",
+						left: cards[key].x + "px",
+					}}
+					draggable={true}
+					onDragStart={(e) => setDragging({ key, x:e.clientX - cards[key].x, y: e.clientY - cards[key].y})}
+					ref={ref}
+				>
+					{editMode.key === key ? (
+						<textarea
+							onBlur={(e) => setEditMode({ key: "" })}
+							onChange={(e) => update(key, { ...cards[key], t: e.target.value })}
+							defaultValue={cards[key].t}
+						/>
+					) : (
+						<div onClick={(e) => setEditMode({ key })}>
+							{cards[key].t}
+						</div>
+					)}
+				</div>
+			))}
+
 		</div>
 
 	);
